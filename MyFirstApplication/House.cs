e92721a6ed106d4db5c4426312fb7ba42d5b5627
Tony Perkins <falknferno81@gmail.com>
2023-04-23 16:18:14 -0500

    CAC-103 C# Overview

---
+++ MyFirstApplication/House.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MyFirstApplication;
+
+public class House
+{
+    string foundation;
+    string roofType;
+    string doorColor;
+    int windowSize;
+
+    public void DoorOpenClose()
+    {
+        Console.WriteLine("MY door is open.");
+    }
+}
+
