d6868e7cdbfeea2c5cd965cbf94ca60df759fdba
Tony Perkins <falknferno81@gmail.com>
2023-04-23 20:12:11 -0500

    MyFirstLab

---
+++ MyLocalFunction/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyLocalFunction;
+
+/*This local function is 
+  for my first exercise lab.
+ */
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This is my first sentence");
+Console.WriteLine("I am new to CSharp");
+Console.WriteLine("This CSharp course is cool");
+Console.WriteLine("I am learning stuff every day.");
+
+void MyLocalFunction()
+{
+
+}
