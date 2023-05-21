cb13578d1805853c6c6345713b46ecd91fb8729f
Anthony Perkins <falknferno@yahoo.com>
2023-05-21 05:45:49 -0500

    CAC-103 Excercise1 Intergals

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using Exercises;
+using MyFirstApplication;
+
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// The below statement calls a local function
+// single line comment
+
+
+/*
+ Multi-line comment
+The below statement is a local function. Local functions
+are inside of members and are private by default.  This means
+they are specific to that member and can only be called 
+inside that member.
+ */
+
+// Lesson Examples
+// HouseExample();
+// Lesson1Example();
+// Lesson2Example();
+// Exercise1Example();
+
+// Exercise 1
+void Exercise1Example()
+{
+    Exercise1 myExercise = new Exercise1();
+    myExercise.IntegralExample();
+    myExercise.IntFromByte();
+    myExercise.LongFromShort();
+    myExercise.FloatFromInt();
+    myExercise.ConvertDoubleToLong();
+    myExercise.ConvertFloatToInt();
+    myExercise.ConvertLongToShort();
+    myExercise.Integrals();
+
+}
+
+// Lesson 2 Strings
+void Lesson2Example()
+{
+    Lesson2 myLesson2 = new Lesson2();
+    myLesson2.MyTrimExample();
+    int able = myLesson2.MyStringLength();
+    Console.WriteLine(able);
+    myLesson2.MyEqualsExample("Hello World");
+    myLesson2.MyExampleChar();
+    myLesson2.MyEscapeExample();
+    Console.WriteLine(myLesson2.MyEscapeExample());
+    myLesson2.MyJoinedStrings("CSharp", "Rules");
+    Console.WriteLine(myLesson2.MyInterpolationExample("pizza",3));
+    myLesson2.MyOtherInterpolation();
+}
+
+void HouseExample()
+{
+    House myHouse = new House();
+    myHouse.DoorOpenClose();
+}
+
+// Lesson 1 Value Types
+void Lesson1Example()
+{
+    Lesson1 myLesson = new Lesson1();
+    myLesson.IntMinMax();
+    myLesson.IntegralExample();
+    myLesson.ConvertFloatToInt();
+    myLesson.LongFromInt();
+
+}
+
