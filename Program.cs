f92a37a880e0fb276a7de075ece3dff6686d0b97
Anthony Perkins <falknferno@yahoo.com>
2023-05-23 15:21:55 -0500

    CAC-103 Excercise1 Intergals

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Exercises;
+﻿using DocumentFormat.OpenXml.InkML;
+using MyFirstApplication;
 using MyFirstApplication;
 
 // See https://aka.ms/new-console-template for more information
@@ -21,20 +22,49 @@ inside that member.
 // Lesson1Example();
 // Lesson2Example();
 // Exercise1Example();
+// Exercise2Example();
+// Lesson3Example()
+
+// Lesson3 Operators and Overflow Checking
+void Lesson3Example()
+{
+    Lesson3 myLesson3 = new Lesson3();
+    // myLesson3.BasicCheckedOperator(14);
+    // myLesson3.BasicCheckedExample2();
+    // int alpha = 2147483647;
+    // int total = alpha + 1;
+    // Console.WriteLine(total);
+    // -2147483639
+}
+
+
+// Exercise 2
+void Exercise2Example()
+{
+    Exercise2 myExercise2 = new Exercise2();
+    myExercise2.myTrimExample();
+    int able = myExercise2.MyStringLength();
+    Console.WriteLine(able);
+    myExercise2.MyContainsExample("Hello World");
+    myExercise2.MyExampleChar();
+    myExercise2.MyEscapeExample();
+    Console.WriteLine(myExercise2.MyEscapeExample());
+    Console.WriteLine(myExercise2.MyInterpolationExample("Anthony", "The Matrix", "pizza rolls", "root beer"));
+
+}
 
 // Exercise 1
 void Exercise1Example()
 {
-    Exercise1 myExercise = new Exercise1();
-    myExercise.IntegralExample();
-    myExercise.IntFromByte();
-    myExercise.LongFromShort();
-    myExercise.FloatFromInt();
-    myExercise.ConvertDoubleToLong();
-    myExercise.ConvertFloatToInt();
-    myExercise.ConvertLongToShort();
-    myExercise.Integrals();
-
+    Exercise1 myExercise1 = new Exercise1();
+    myExercise1.IntegralExample();
+    myExercise1.IntFromByte();
+    myExercise1.LongFromShort();
+    myExercise1.FloatFromInt();
+    myExercise1.ConvertDoubleToLong();
+    myExercise1.ConvertFloatToInt();
+    myExercise1.ConvertLongToShort();
+    myExercise1.Integrals();
 }
 
 // Lesson 2 Strings
@@ -49,7 +79,7 @@ void Lesson2Example()
     myLesson2.MyEscapeExample();
     Console.WriteLine(myLesson2.MyEscapeExample());
     myLesson2.MyJoinedStrings("CSharp", "Rules");
-    Console.WriteLine(myLesson2.MyInterpolationExample("pizza",3));
+    Console.WriteLine(myLesson2.MyInterpolationExample("pizza", 3));
     myLesson2.MyOtherInterpolation();
 }
 
