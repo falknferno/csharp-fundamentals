eca97878a31b274997e84a0a7c2b2fc7207d27bf
Anthony Perkins <falknferno@yahoo.com>
2023-06-08 16:11:54 -0500

    CAC-103 Constructor Exercise

---
+++ MyFirstApplication/Exercises/Exercise6.cs
@@ -0,0 +1,27 @@
+﻿
+namespace MyFirstApplication;
+
+internal class Exercise6
+{
+    private string _shoeType;
+    private int _shoeSize;
+
+    public string ShoeType
+    {
+        get { return _shoeType; }
+        set { _shoeType = value; }
+    }
+
+    public int ShoeSize
+    {
+        get { return _shoeSize; }
+        set { _shoeSize = value; }
+    }
+
+    public Exercise6(string shoeType, int shoeSize)
+    {
+        ShoeType = shoeType;
+        ShoeSize = shoeSize;
+    }
+}
+
--- MyFirstApplication/Program.cs
+++ MyFirstApplication/Program.cs
@@ -27,50 +27,59 @@
 // Exercise4Example();
 // Lesson5Example();
 // Exercise5Example();
-Lesson6Example();
+// Lesson6Example();
+Exercise6Example();
+
+void Exercise6Example()
+{
+    Exercise6 myshoes = new Exercise6("Nike", 12);
+    Console.WriteLine(myshoes.ShoeSize);
+    myshoes.ShoeSize = 12;
+}
 
 
 void Lesson6Example()
+
 {
     // Default Constructor
-    Lesson6 myLesson6 = new Lesson6();
-    Lesson6 myLessonHats = new Lesson6("Cowboy", 7);
-    Lesson6.TryOn theHat = myLessonHats.TryOnHat;
-    theHat($"I tried on a {myLessonHats.HatType} hat that was size {myLessonHats.HatSize}");
+    // Lesson6 myLesson6 = new Lesson6();
+    // Lesson6 myLessonHats = new Lesson6("Cowboy", 7);
+    // Lesson6.TryOn theHat = myLessonHats.TryOnHat;
+    // theHat($"I tried on a {myLessonHats.HatType} hat that was size {myLessonHats.HatSize}");
 
-    Console.WriteLine();  /*  used as a space between the delegate above and the 
-                                       the stuff that prints out below
-                           */
+    // Console.WriteLine();    used as a space between the delegate above and the 
+                                // the stuff that prints out below 
+                                
 
     // Constructor with 1 parameter
-    Lesson6 myOther6 = new Lesson6(22);
+    // Lesson6 myOther6 = new Lesson6(22);
 
     // Constructor with 2 parameters
-    Lesson6 myHats = new Lesson6("Cowboy", 7); 
-    Console.WriteLine(myHats.HatSize);  // getting value
-    myHats.HatSize = 6; // setting value
+    // Lesson6 myHats = new Lesson6("Cowboy", 7); 
+    // Console.WriteLine(myHats.HatSize);  // getting value
+    // myHats.HatSize = 6; // setting value
 
     // Method Parameter Modifiers
-    int able = 33, beta = 22, charlie;
+    // int able = 33, beta = 22, charlie;
 
-    myLesson6.InExample(able);
-    myLesson6.RefExample(ref beta);
-    Console.WriteLine(beta);
-    myLesson6.OutExample(out charlie);
-    Console.WriteLine(charlie);
+    // myLesson6.InExample(able);
+    // myLesson6.RefExample(ref beta);
+    // Console.WriteLine(beta);
+    // myLesson6.OutExample(out charlie);
+    // Console.WriteLine(charlie);
 
-    Console.WriteLine();
+    // Console.WriteLine();
 
     // Mult-cast Delegate
     Lesson6.TryOn theNewHat, hangTheHat, multiHat;
 
-    theNewHat = myLesson6.TryOnHat;
-    hangTheHat = myLesson6.HangUpHat;
-    multiHat = theNewHat + hangTheHat;
+    // theNewHat = myLesson6.TryOnHat;
+    // hangTheHat = myLesson6.HangUpHat;
+    // multiHat = theNewHat + hangTheHat;
 
-    theNewHat($"Trying a {myLessonHats.HatType} hat");
-    hangTheHat($"Hanging up my {myLessonHats.HatType} hat");
-    multiHat($"My {myLessonHats.HatType} hat");
+    // theNewHat($"Trying a {myLessonHats.HatType} hat");
+    // hangTheHat($"Hanging up my {myLessonHats.HatType} hat");
+    // multiHat($"My {myLessonHats.HatType} hat");
 
 }
 
