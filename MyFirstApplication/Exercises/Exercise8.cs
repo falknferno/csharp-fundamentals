7f6f9f4fd3f5e10512643ab200c9e26b8a2d834f
Anthony Perkins <falknferno@yahoo.com>
2023-06-16 12:55:55 -0500

    CAC-103 Exercise 8

--- MyFirstApplication/Exercises/Exercise8.cs
+++ MyFirstApplication/Exercises/Exercise8.cs
@@ -27,6 +27,19 @@ internal class Exercise8
         CalculateInchCount();
     }
 
+    public static void RectangleAndSquare()
+    {
+        int length = 8;
+        int width = 6;
+        int area = length * width;
+        Console.WriteLine($"The area {length} and {width} is {area}");
+
+        /*
+         Used a static method and int values to come up with the area of the rectangle/square, with the int value of the area.
+         Then used a void return with an interpolation to tell me when I ran it in program.cs.
+         */
+    }
+
 
 }
 
