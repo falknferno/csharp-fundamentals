edb74656954e791a5067ecd889c1401cf1f9b0b4
Anthony Perkins <falknferno@yahoo.com>
2023-07-08 09:55:57 -0500

    CAC-103 Exercise 12 Updates

--- MyFirstApplication/Exercises/Boat.cs
+++ MyFirstApplication/Exercises/Boat.cs
@@ -16,8 +16,28 @@ public class Boat
 
     public string BoatType
     {
-        get => _boa
+       get => _boatType;
+       set => _boatType = value;
     }
+
+    public string Travels
+    {
+        get => _travels;
+        set => _travels = value;
+    }
+
+    public string BoatName
+    {
+        get => _boatName;
+        set => _boatName = value;
+    }
+
+    public virtual void BoatCruises()
+    {
+        Console.WriteLine($"My boat moves {Travels} by rowing with oars.");
+    }
+
+
 
 
 }// end class
---
+++ MyFirstApplication/Exercises/Horses.cs
@@ -0,0 +1,42 @@
+﻿namespace MyFirstApplication;
+
+public class Horses
+{
+    public string Breed;
+    public string MoveType;
+    public string HairColors;
+    public string NumberOfAge;
+
+    public Horses(string breed, string moveType, string HairColors, string numberOfAge)
+    {
+        this.Breed = breed;
+        this.MoveType = moveType;
+        this.HairColors = HairColors;
+        this.NumberOfAge = numberOfAge;
+    }
+
+}// end class
+
+public class Foal : Horses
+{
+    public string Age { get; set; }
+       
+
+    public Foal(string breed, string moveType, string hairColors, string age)
+        :base(breed, moveType, hairColors, "colt")
+    {
+        Age = age;
+    }
+
+    public Foal(string breed, string moveType)
+        :this(breed, moveType, "brown and grey", "colt")
+    {}
+
+    public Foal()
+        :this("thoroughbred", "gallop")
+    {}
+
+}// end class
+    
+
+
