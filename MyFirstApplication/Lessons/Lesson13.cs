6b30551f11b43de92ad4ada450e32f8a28089d50
falknferno <130600888+falknferno@users.noreply.github.com>
2023-07-08 12:49:09 -0500

    Add files via upload

---
+++ MyFirstApplication/Lessons/Lesson13.cs
@@ -0,0 +1,51 @@
+﻿namespace MyFirstApplication;
+
+/*
+ This will be about Abstract modifier and Interfaces
+ */
+internal abstract class Lesson13Base
+{
+    protected int num;
+
+    // Abstract properties
+
+    protected abstract int Inches { get; }
+    protected abstract int Feet { get; }
+
+    protected Lesson13Base(int num)
+    {
+        this.num = num;
+    }
+
+    // Abstract Methods
+    public abstract int ConvertFeetToInches();
+    public abstract int ConvertYardsToFeet();
+
+}// end class
+
+/*
+ Our derived class will implement our Abstract class members
+ */
+
+internal class Lesson13Derived: Lesson13Base  // derived will also ask for constructor, as we do not have a default constructor
+{
+    // implementing abstract properties
+    protected override int Inches { get; } = 12;
+    protected override int Feet { get; } = 3;
+
+    public Lesson13Derived(int value)
+        :base(value) { }
+
+    // Implementing abstract methods
+    public override int ConvertFeetToInches()
+    {
+        return num * Inches;
+    }
+
+    public override int ConvertYardsToFeet()
+    {
+        return num * Feet;
+    }
+
+
+}// end class
---
+++ MyFirstApplication/Lessons/Lesson7Record.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace MyFirstApplication;
+
+/*
+ record is a reference type that provides built-in functionality
+ for encapsulationg data
+ */
+internal record Person(string firstName, string lastName);
+
+/*
+  a recort struct is a value type with similar functionality as a record class
+ */
+public record struct Resolution(int width, int height)
+{
+    public void CalculateRes()
+    {
+        Console.WriteLine(width * height);
+    }
+}
