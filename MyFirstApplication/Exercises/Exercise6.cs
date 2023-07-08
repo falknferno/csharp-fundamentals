ec5935ba9b779e7561d0985b8b06f59dacd7dd21
falknferno <130600888+falknferno@users.noreply.github.com>
2023-07-08 12:34:52 -0500

    Add files via upload

---
+++ MyFirstApplication/Exercises/Exercise6.cs
@@ -0,0 +1,42 @@
+﻿
+namespace MyFirstApplication;
+
+public class Exercise6
+{
+    private string _shoeType;
+    private int _shoeSize;
+
+    public string ShoeType
+    {
+        get { return _shoeType; }
+        set { _shoeType = value; }
+    }
+
+    public int ShoeSize
+    {
+        get { return _shoeSize; }
+        set { _shoeSize = value; }
+    }
+
+    public Exercise6(string shoeType, int shoeSize)
+    {
+        _shoeType = shoeType;
+        _shoeSize = shoeSize;
+    }
+
+
+    public delegate void TryOn(string type);
+
+    public void TryOnShoe(string message)
+    {
+        Console.WriteLine($"TryOnShoe = {message}");
+
+    }
+    /*
+      I used a constructor method, a reference type (delegate) in my method, string 
+      and int parameters and my void return type, using Console.WriteLine 
+     */
+
+
+}
+
