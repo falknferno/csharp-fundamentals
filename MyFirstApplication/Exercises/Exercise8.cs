ba83aa057109390d99b78a2c06a9cae221236735
falknferno <130600888+falknferno@users.noreply.github.com>
2023-07-08 12:38:28 -0500

    Add files via upload

---
+++ MyFirstApplication/Exercises/Exercise8.cs
@@ -0,0 +1,46 @@
+﻿namespace MyFirstApplication;
+
+internal class Exercise8
+{
+    public const int Feet = 3;
+    public const int Inches = 36;
+
+    public const double InchesPerFoot = (double)Inches / (double)Feet;
+
+    public static int InchCount;
+    public int _inches;
+
+    public Exercise8(int lengthInches)
+    {
+        _inches = lengthInches;
+    }
+
+    public static int CalculateInchCount()
+    {
+        return ++InchCount;
+    }
+
+    public void CalculateInches()
+    {
+        int total = Feet * _inches;
+        Console.WriteLine($"The total number of {_inches} is {total}");
+        CalculateInchCount();
+    }
+
+    public static void RectangleAndSquare()
+    {
+        int length = 8;
+        int width = 6;
+        int area = length * width;
+        Console.WriteLine($"The area of {length} and {width} is {area}");
+
+        /*
+         Used a static method and int values to come up with the area of the rectangle/square, with the int value of the area.
+         Then used a void return with an interpolation to tell me when I ran it in program.cs.
+         */
+    }
+
+
+}
+
+
