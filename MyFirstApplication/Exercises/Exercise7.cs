1412aca62d0b8bb511f819afb483dc66232ac00b
falknferno <130600888+falknferno@users.noreply.github.com>
2023-07-08 12:35:45 -0500

    Add files via upload

---
+++ MyFirstApplication/Exercises/Exercise7.cs
@@ -0,0 +1,35 @@
+﻿namespace MyFirstApplication;
+
+internal struct Employee
+{
+    public int _id;
+    public string _name;
+
+    public int Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+
+    public Employee(int id, string name)
+    {
+        _id = id;
+        _name = name;
+    }
+
+    public override string ToString()
+    {
+        return _name;
+    }
+
+   
+
+}
+
+
