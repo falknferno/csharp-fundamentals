c01e399dd7b884902deea41f352c9d070d7a086e
Anthony Perkins <falknferno81@gmail.com>
2023-09-01 19:25:26 -0500

    CAC-103 Exercise 15 Eums and Tuples

---
+++ MyFirstApplication/Exercises/Exercise15.cs
@@ -0,0 +1,29 @@
+﻿namespace MyFirstApplication;
+
+public enum FavoriteMovies { Superman, Matrix, Transformers, Thor, Bleach }
+
+public class Exercise15
+{
+    public string ActionMovies(FavoriteMovies movies)
+    {
+        string message = movies switch
+        {
+            FavoriteMovies.Superman => $"My favorite movie is {FavoriteMovies.Superman}",
+            FavoriteMovies.Matrix => $"My favorite movie is {FavoriteMovies.Matrix}",
+            FavoriteMovies.Transformers => $"My favorite movie is {FavoriteMovies.Transformers}",
+            FavoriteMovies.Thor => $"My favorite movie is {FavoriteMovies.Thor}",
+            _ => $"My favorite movie is {FavoriteMovies.Bleach}"
+        };
+        return message;
+    } 
+
+
+    public (int, string) AwesomeMovies (FavoriteMovies movies)
+    {
+        string fm = movies.ToString();
+        int able = (int)movies;
+        return (able, fm);
+    }
+    
+    
+} // end class
